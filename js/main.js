32472d0375c086cf595f5c9c491bb1eb67bf359a
Fralch <fralch@gmail.com>
2016-01-15 18:34:22 -0500

    ya por terminar

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 if (screen.width>=1024) {
-	 $("body").css("background-color", "red");
+	 $( "<div id='botonregis'><a href='#'>REGISTRA TU CANCHITA</a></div>" ).insertAfter( ".wrapper" );
+	 $( "<div id='social'><a href='#'><img src='img/fb.svg'/></a><a href='#'><img src='img/plus.svg'/></a> <a href='#'> <img id='tw' src='img/twitter.svg'/></a></div>" ).insertAfter( ".wrapper" );
 	 console.log("hola")
+
 };
